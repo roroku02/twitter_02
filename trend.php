b3664232575701b8bd2e25f3731e7534baa673a3
roroku02 <roroku02@gmail.com>
2018-09-14 17:50:39 +0900

    検索ジャンル表示機能

--- trend.php
+++ trend.php
@@ -20,7 +20,10 @@
     <title>トレンドワード選択</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" media="screen" href="css/style.css" />
-    <script src="js/main.js"></script>
+    <script type="text/javascript" src="js/jquery-3.3.1.min.js"></script>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp"
+        crossorigin="anonymous">
+
 </head>
 <body>
 <a href="#" onclick="javascript:window.history.back(-1);return false;">前のページに戻る</a>
@@ -51,4 +54,6 @@
         ?>
     </section>
 </body>
+
+<script src="js/main.js"></script>
 </html>
